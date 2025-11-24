e00e09a9f5c4f2bb66b23ac01a63eafb9ffcb5e3
Lê Nguyễn Bảo Minh <lenguyenbaominh1234@gmail.com>
2025-11-24 10:28:22 +0700

    init backend

---
+++ backend/src/main/java/com/project/softwareengbackend/SoftwareengbackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.softwareengbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareengbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareengbackendApplication.class, args);
+	}
+
+}
